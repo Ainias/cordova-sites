1ff24712cbd821821d785a673c870b58485a23a9
sguenter <sguenther@churchtools.de>
2024-07-06 20:42:18 +0200

    update new version

--- src/shared/helper/redirectFromInitialProps.ts
+++ src/shared/helper/redirectFromInitialProps.ts
@@ -4,8 +4,10 @@ import Router from 'next/router';
 export function redirectFromInitialProps(newLocation: string, res?: ServerResponse): Promise<any> {
     if (res) {
         res.writeHead(307, { Location: newLocation });
-        res.end();
-        return new Promise<any>(() => {});
+
+        return new Promise<any>((r) => {
+            res.end(r);
+        });
     }
     return Router.replace(newLocation) as Promise<any>;
 }
